8a80cfb1b1bf5be0b9421e5f24adde45f6d5d6d6
abhisheksonii <abhisir75@gmail.com>
2024-01-15 23:39:35 +0530

    added the openAI utilizes langchain based Streamlit APP

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # streamlit-wizardry
+
+
+## There's a lot more to build with streamlit but for now let's build something useful to me
+
+### OpenAI Based Youtube Summarizer
+
+Next task - Using Transformers instead of OpenAI
---
+++ youtube summarizer/main.py
@@ -0,0 +1,38 @@
+from langchain.document_loaders import youtube
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from youtube_transcript_api import YouTubeTranscriptApi
+
+import streamlit as st
+
+
+st.set_page_config("YT Summarizer")
+st.header = "Your Personal YT summarizer"
+
+url = st.text_input("Enter URL of the youtube video")
+
+if st.button("Submit", type="primary"):
+    if url is not None:
+        print(url)
+        loader =  youtube.YoutubeLoader.from_youtube_url(url)
+        docs = loader.load()
+        # print(len(docs[0].page_content)) #to know how many characters are there
+
+        ts = RecursiveCharacterTextSplitter(chunk_size = 6000, chunk_overlap = 0)
+        final_doc = ts.split_documents(docs)
+
+        # print(len(final_doc))
+        temp = []
+        for part in final_doc:
+            # print(part,end="\n\n")
+            openai.api_key = "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"
+            response = openai.chat.completions.create(
+                model = "gpt-3.5-turbo",
+                messages = [
+                    {"role" :"system","content":"You are an advanced AGI assistant" },
+                    {"role" :"user","content":f"summarize the following in to bullet points and make it more simpler{part}" }
+                ]
+            )
+            # print(response)
+            msg = response["choices"][0]["messages"]["content"]
+            temp.append(msg)
+        st.write("".join(temp))
